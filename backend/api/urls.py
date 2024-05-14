04013fb767d4b91e305014671225dabbbc4ba703
Ahmed Alsaedi <aalsaedi265@gmail.com>
2024-05-14 14:10:04 -0400

    django

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("note/", views.NoteListCreate.as_view(), name="note-list"),
+    path("notes/", views.NoteListCreate.as_view(), name="note-list"),
     #<int:pk? >/ means primary key
-    path("notes/delete/<int:pk>/", views.NoteDelete.as_view(), name="delete-note")
+    path("notes/delete/<int:pk>/", views.NoteDelete.as_view(), name="delete-note"),
 ]
--- backend/api/views.py
+++ backend/api/views.py
@@ -1,42 +1,36 @@
-
 from django.shortcuts import render
 from django.contrib.auth.models import User
 from rest_framework import generics
-from .serilalizers import UserSerializer, NoteSerializer
+from .serializers import UserSerializer, NoteSerializer
 from rest_framework.permissions import IsAuthenticated, AllowAny
 from .models import Note
 
-# Create your views here
 
 class NoteListCreate(generics.ListCreateAPIView):
-    #will list all created node or create a node
     serializer_class = NoteSerializer
     permission_classes = [IsAuthenticated]
-    
+
     def get_queryset(self):
         user = self.request.user
-         #for all node, super waist of electricity do this Note.objects.all()
-         #below is my specific author
-        return Note.objects.filter(author = user)
-    
+        return Note.objects.filter(author=user)
+
     def perform_create(self, serializer):
         if serializer.is_valid():
-            serializer.save(author = self.request.user)
+            serializer.save(author=self.request.user)
         else:
             print(serializer.errors)
+
 
 class NoteDelete(generics.DestroyAPIView):
     serializer_class = NoteSerializer
     permission_classes = [IsAuthenticated]
-    
+
     def get_queryset(self):
         user = self.request.user
-        return Note.objects.filter(author = user)
-    
+        return Note.objects.filter(author=user)
+
 
 class CreateUserView(generics.CreateAPIView):
-    
-    queryset = User.objects.all() #make sure we don't make new user
-    serializer_class = UserSerializer #what is accaptable to make new user class
-    permission_classes = [AllowAny] #allows non auths to create new user
-    
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [AllowAny]
