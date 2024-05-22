9e268f1504fa7f4996bda04e2914ece35110b3a8
Ahmed Alsaedi <aalsaedi265@gmail.com>
2024-05-22 14:52:13 -0400

    routers

--- frontend/src/components/ProtectedRoute.jsx
+++ frontend/src/components/ProtectedRoute.jsx
@@ -1,57 +1,57 @@
-
 import { Navigate } from "react-router-dom";
 import { jwtDecode } from "jwt-decode";
 import api from "../api";
 import { REFRESH_TOKEN, ACCESS_TOKEN } from "../constants";
 import { useState, useEffect } from "react";
-// protects route meaning need access token to get to route
+
 
 function ProtectedRoute({ children }) {
-    const [isAuthorized, setIsAuthorized] = useState(null)
-    
+    const [isAuthorized, setIsAuthorized] = useState(null);
+
     useEffect(() => {
-        auth().catch(()=> setIsAuthorized(false))
-    },[])
+        auth().catch(() => setIsAuthorized(false))
+    }, [])
 
     const refreshToken = async () => {
-        const refreshToken = localStorage.getItem(REFRESH_TOKEN)
+        const refreshToken = localStorage.getItem(REFRESH_TOKEN);
         try {
             const res = await api.post("/api/token/refresh/", {
                 refresh: refreshToken,
-            })
+            });
             if (res.status === 200) {
                 localStorage.setItem(ACCESS_TOKEN, res.data.access)
+                setIsAuthorized(true)
             } else {
                 setIsAuthorized(false)
             }
         } catch (error) {
-            console.log(error)
-            setIsAuthorized(false)
+            console.log(error);
+            setIsAuthorized(false);
         }
-    }
-//check if expired or not , if exp refresh
+    };
+
     const auth = async () => {
-        //localstorage allows for presistence in the browser
-        const token = localStorage.getItem(ACCESS_TOKEN)
+        const token = localStorage.getItem(ACCESS_TOKEN);
         if (!token) {
-            setIsAuthorized(false)
-            return
+            setIsAuthorized(false);
+            return;
         }
-        const decode = jwtDecode(token)
-        const tokenExpiration = decode.exp
-        const now = Date.now / 1000 // get in sec instead of msec
+        const decoded = jwtDecode(token);
+        const tokenExpiration = decoded.exp;
+        const now = Date.now() / 1000;
 
         if (tokenExpiration < now) {
-            await refreshToken()
+            await refreshToken();
         } else {
-            setIsAuthorized(true)
+            setIsAuthorized(true);
         }
+    };
 
-    }
-//checks if you got token if not will be redirected
     if (isAuthorized === null) {
-        return <div>LOADING ...</div>
+        return <div>Loading...</div>;
     }
-    return isAuthorized ? children : <Navigate to="/login" />
+
+    return isAuthorized ? children : <Navigate to="/login" />;
 }
-export default ProtectedRoute
+
+export default ProtectedRoute;
